6b3448268f65a837884ffa44fb5ac1f38203bba8
Gerardo Cicalese <92589816+mazamin7@users.noreply.github.com>
2023-05-01 00:00:29 +0200

    Working damped DD and removed FEM

--- code_modular.m
+++ code_modular.m
@@ -6,13 +6,11 @@ opts = ["Pre 7 points" "Post 7 points"];
 choice = menu(msg, opts);
 
 msg2 = "Choose the update for left";
-opts2 = ["FDTD" "Fourier" "FEM" "PML"];
+opts2 = ["FDTD" "Fourier" "PML"];
 choice2 = menu(msg2, opts2);
 
 msg3 = "Choose the update for right";
 choice3 = menu(msg3, opts2);
-
-assert(choice2 ~= 3 && choice3 ~= 3, 'FEM not supported');
 
 % Simulation parameters
 c0 = 1;
@@ -21,7 +19,7 @@ T_sec = 10; % Simulation duration
 alpha_abs_left = 0; % Absorption coefficient
 alpha_abs_right = 0;
 dh = 0.1;
-dt = 0.02;
+dt = 0.005;
 transmittivity = 1; % Transmittance of the middle boundary
 
 assert(dt < dh / 2 / c0);
@@ -70,20 +68,16 @@ force_pos = len_x * source_pos_ratio_x;
 force_envelope = @(x,mu) 1/(sigma * sqrt(2 * pi)) * exp(-(x-mu).^2/(2*sigma^2)); % Gaussian function
 
 % Initializing update methods
-if choice2 == 1 || choice2 == 4
+if choice2 == 1 || choice2 == 3
     FDTD_data_left = init_FDTD(len_x/2, c0, dt, dh, left_damped, alpha_abs, bc_left, "N", choice2 == 4);
 elseif choice2 == 2
     Fourier_data_left = init_Fourier(len_x/2, c0, dt, dh, left_damped, alpha_abs_left);
-else
-    FEM_data_left = init_FEM(len_x/2, c0, dt, dh, left_damped, alpha_abs, bc_left, "N");
 end
 
-if choice3 == 1 || choice3 == 4
+if choice3 == 1 || choice3 == 3
     FDTD_data_right = init_FDTD(len_x/2, c0, dt, dh, right_damped, alpha_abs, "N", bc_right, choice3 == 4);
 elseif choice3 == 2
     Fourier_data_right = init_Fourier(len_x/2, c0, dt, dh, right_damped, alpha_abs_right);
-else
-    FEM_data_right = init_FEM(len_x/2, c0, dt, dh, right_damped, alpha_abs, "N", bc_right);
 end
 
 % Simulation loop
@@ -114,21 +108,17 @@ for n = 1:N_t
     end
     
     % Update left
-    if choice2 == 1 || choice2 == 4
+    if choice2 == 1 || choice2 == 3
         p_next(1:N_x/2) = update_FDTD(FDTD_data_left, p_curr(1:N_x/2), p_prev(1:N_x/2), force(1:N_x/2), g1, 0);
     elseif choice2 == 2
         [p_next(1:N_x/2),q_next(1:N_x/2)] = update_Fourier(Fourier_data_left, p_curr(1:N_x/2), p_prev(1:N_x/2), force(1:N_x/2), q_curr(1:N_x/2), q_prev(1:N_x/2));
-    else
-        p_next(1:N_x/2) = update_FEM(FEM_data_left, p_curr(1:N_x/2), p_prev(1:N_x/2), force(1:N_x/2));
     end
     
     % Update right
-    if choice3 == 1 || choice3 == 4
+    if choice3 == 1 || choice3 == 3
         p_next(N_x/2+1:N_x) = update_FDTD(FDTD_data_right, p_curr(N_x/2+1:N_x), p_prev(N_x/2+1:N_x), force(N_x/2+1:N_x), 0, g2);
     elseif choice3 == 2
         [p_next(N_x/2+1:N_x),q_next(N_x/2+1:N_x)] = update_Fourier(Fourier_data_right, p_curr(N_x/2+1:N_x), p_prev(N_x/2+1:N_x), force(N_x/2+1:N_x), q_curr(N_x/2+1:N_x), q_prev(N_x/2+1:N_x));
-    else
-        p_next(N_x/2+1:N_x) = update_FEM(FEM_data_right, p_curr(N_x/2+1:N_x), p_prev(N_x/2+1:N_x), force(N_x/2+1:N_x));
     end
 
     % Post-merge
