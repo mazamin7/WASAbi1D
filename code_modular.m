391d40f31330336e71849a80a1ec50ded3c17d7a
Gerardo Cicalese <92589816+mazamin7@users.noreply.github.com>
2023-05-01 00:26:33 +0200

    Implemented first order FDTD

--- code_modular.m
+++ code_modular.m
@@ -6,7 +6,7 @@ opts = ["Pre 7 points" "Post 7 points"];
 choice = menu(msg, opts);
 
 msg2 = "Choose the update for left";
-opts2 = ["FDTD" "Fourier" "PML"];
+opts2 = ["FDTD 2ord" "Fourier" "PML" "FDTD 1ord"];
 choice2 = menu(msg2, opts2);
 
 msg3 = "Choose the update for right";
@@ -34,7 +34,19 @@ bc_right = "N";
 left_damped = true;
 right_damped = true;
 
-assert(left_damped == right_damped);
+if (left_damped == true && choice2 == 2) || (choice2 == 4)
+    order_left = 1;
+else
+    order_left = 2;
+end
+
+if (right_damped == true && choice3 == 2) || (choice3 == 4)
+    order_right = 1;
+else
+    order_right = 2;
+end
+
+assert(order_left == order_right);
 % otherwise domain decomposition doesn't work
 
 % Source
@@ -69,15 +81,19 @@ force_envelope = @(x,mu) 1/(sigma * sqrt(2 * pi)) * exp(-(x-mu).^2/(2*sigma^2));
 
 % Initializing update methods
 if choice2 == 1 || choice2 == 3
-    FDTD_data_left = init_FDTD(len_x/2, c0, dt, dh, left_damped, alpha_abs, bc_left, "N", choice2 == 4);
+    data_left = init_FDTD(len_x/2, c0, dt, dh, left_damped, alpha_abs_left, bc_left, "N", choice2 == 3);
 elseif choice2 == 2
-    Fourier_data_left = init_Fourier(len_x/2, c0, dt, dh, left_damped, alpha_abs_left);
+    data_left = init_Fourier(len_x/2, c0, dt, dh, left_damped, alpha_abs_left);
+elseif choice2 == 4
+    data_left = init_FDTD_1ord(len_x/2, c0, dt, dh, alpha_abs_left);
 end
 
 if choice3 == 1 || choice3 == 3
-    FDTD_data_right = init_FDTD(len_x/2, c0, dt, dh, right_damped, alpha_abs, "N", bc_right, choice3 == 4);
+    data_right = init_FDTD(len_x/2, c0, dt, dh, right_damped, alpha_abs_right, "N", bc_right, choice3 == 3);
 elseif choice3 == 2
-    Fourier_data_right = init_Fourier(len_x/2, c0, dt, dh, right_damped, alpha_abs_right);
+    data_right = init_Fourier(len_x/2, c0, dt, dh, right_damped, alpha_abs_right);
+elseif choice3 == 4
+    data_right = init_FDTD_1ord(len_x/2, c0, dt, dh, alpha_abs_right);
 end
 
 % Simulation loop
@@ -109,23 +125,27 @@ for n = 1:N_t
     
     % Update left
     if choice2 == 1 || choice2 == 3
-        p_next(1:N_x/2) = update_FDTD(FDTD_data_left, p_curr(1:N_x/2), p_prev(1:N_x/2), force(1:N_x/2), g1, 0);
+        p_next(1:N_x/2) = update_FDTD(data_left, p_curr(1:N_x/2), p_prev(1:N_x/2), force(1:N_x/2), g1, 0);
     elseif choice2 == 2
-        [p_next(1:N_x/2),q_next(1:N_x/2)] = update_Fourier(Fourier_data_left, p_curr(1:N_x/2), p_prev(1:N_x/2), force(1:N_x/2), q_curr(1:N_x/2), q_prev(1:N_x/2));
+        [p_next(1:N_x/2),q_next(1:N_x/2)] = update_Fourier(data_left, p_curr(1:N_x/2), p_prev(1:N_x/2), force(1:N_x/2), q_curr(1:N_x/2), q_prev(1:N_x/2));
+    elseif choice2 == 4
+        [p_next(1:N_x/2),q_next(1:N_x/2)] = update_FDTD_1ord(data_left, p_curr(1:N_x/2), p_prev(1:N_x/2), force(1:N_x/2), q_curr(1:N_x/2), q_prev(1:N_x/2));
     end
     
     % Update right
     if choice3 == 1 || choice3 == 3
-        p_next(N_x/2+1:N_x) = update_FDTD(FDTD_data_right, p_curr(N_x/2+1:N_x), p_prev(N_x/2+1:N_x), force(N_x/2+1:N_x), 0, g2);
+        p_next(N_x/2+1:N_x) = update_FDTD(data_right, p_curr(N_x/2+1:N_x), p_prev(N_x/2+1:N_x), force(N_x/2+1:N_x), 0, g2);
     elseif choice3 == 2
-        [p_next(N_x/2+1:N_x),q_next(N_x/2+1:N_x)] = update_Fourier(Fourier_data_right, p_curr(N_x/2+1:N_x), p_prev(N_x/2+1:N_x), force(N_x/2+1:N_x), q_curr(N_x/2+1:N_x), q_prev(N_x/2+1:N_x));
+        [p_next(N_x/2+1:N_x),q_next(N_x/2+1:N_x)] = update_Fourier(data_right, p_curr(N_x/2+1:N_x), p_prev(N_x/2+1:N_x), force(N_x/2+1:N_x), q_curr(N_x/2+1:N_x), q_prev(N_x/2+1:N_x));
+    elseif choice3 == 4
+        [p_next(N_x/2+1:N_x),q_next(N_x/2+1:N_x)] = update_FDTD_1ord(data_right, p_curr(N_x/2+1:N_x), p_prev(N_x/2+1:N_x), force(N_x/2+1:N_x), q_curr(N_x/2+1:N_x), q_prev(N_x/2+1:N_x));
     end
 
     % Post-merge
     if choice == 2
-        if left_damped == 1
+        if order_left == 1
             q_next = q_next + 2 * dt * (c0 / dh)^2 * C * p_curr;
-        else
+        elseif order_left == 2
             p_next = p_next + transmittivity * (c0 * dt / dh)^2 * C * p_curr;
         end
     end
