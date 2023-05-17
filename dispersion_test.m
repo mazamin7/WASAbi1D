34e45d45dce823728a006a5c75c7894d1e53b090
Gerardo Cicalese <92589816+mazamin7@users.noreply.github.com>
2023-05-17 23:27:48 +0200

    Improved dispersion plot

--- dispersion_test.m
+++ dispersion_test.m
@@ -7,7 +7,10 @@ c = test_case_data.c0;
 
 % Simulation parameters
 dh = 1e-3;
-dt = dh * 0.4 / c;
+lambda_arr = [0.1, 0.2, 0.4, 0.6, 0.8];
+plot_m = 3;
+plot_n = 2;
+% lambda_arr = [0.6, 0.8];
 
 % artificial dissipation factors for first order
 xi = 1 - 1e-5;
@@ -19,70 +22,56 @@ nu_fourier = 1 - 1e-10; % - eps(1); % < 1
 % Show debug plot?
 debug = false;
 
-% Run simulation
-[t_axis, x_axis, p, v] = simulation(test_case_data, simulation_parameters, dt, dh, debug, xi, nu, nu_fourier);
-
 len_x = test_case_data.len_x;
 len_t = test_case_data.len_t;
-sigma = 3 * 1/20 * len_x;
+sigma = len_x/80;
 
-figure();
-sgtitle("Wave packet dispersion test");
-
-subplot(211);
 pos_first = len_x/2;
 left_first = pos_first - sigma;
 right_first = pos_first + sigma;
-p_first = p(round(left_first/dh):round(right_first/dh),1);
 N = round(right_first/dh) - round(left_first/dh) + 1;
-plot(x_axis(round(left_first/dh):round(right_first/dh)), p_first);
-xlim([pos_first-len_x/40, pos_first+len_x/40]);
+
+pos_last = len_x/2 + c*1;
+left_last = pos_last - sigma;
+right_last = pos_last + sigma;
+
+p_gt_fun = test_case_data.p_gt_fun;
+N2 = 1e5;
+x_axis2 = linspace(0, len_x, N2);
+dh2 = len_x/N2;
+p_gt = p_gt_fun(x_axis2, 0);
+p_first = p_gt(round(left_first/dh2):round(right_first/dh2));
+
+figure();
+
+subplot(plot_m, plot_n, 1);
+hold on;
+xlim([pos_first-sigma, pos_first+sigma]);
 ylim([0, 12]);
 xlabel("x");
 ylabel(sprintf("p(x,t=%.1f)", 0));
 title(sprintf("Wave packet at t = %.1f", 0));
+plot(x_axis2(round(left_first/dh2):round(right_first/dh2)), p_first);
 
-subplot(212);
-pos_last = len_x/2 + c*1;
-left_last = pos_last - sigma;
-right_last = pos_last + sigma;
-p_last = p(round(left_last/dh):round(right_last/dh),end);
-plot(x_axis(round(left_last/dh):round(right_last/dh)), p_last);
-xlim([pos_last-len_x/40, pos_last+len_x/40]);
-ylim([0, 12]);
-xlabel("x");
-ylabel(sprintf("p(x,t=%.1f)", len_t));
-title(sprintf("Wave packet at t = %.1f", len_t));
+for n = 1:length(lambda_arr)
+    lambda = lambda_arr(n);
+    dt = dh * lambda / c;
+    % Run simulation
+    [t_axis, x_axis, p, v] = simulation(test_case_data, simulation_parameters, dt, dh, debug, xi, nu, nu_fourier);
 
-% fft_len = 8192;
-% % Compute the Fourier transform of the first and last time instant
-% fft_first = fft(p_first,fft_len);
-% fft_last = fft(p_last,fft_len);
-% 
-% % Define the frequency axis
-% f_axis = -fft_len/2:fft_len/2-1;  % Frequency axis
-% 
-% figure;
-% subplot(2, 1, 1);
-% plot(f_axis, abs(fft_first));
-% subplot(2, 1, 2);
-% plot(f_axis, abs(fft_last));
+    p_last = p(round(left_last/dh):round(right_last/dh),end);
 
-% figure;
-% subplot(2, 1, 1);
-% plot(f_axis, ifft(fft_first));
-% subplot(2, 1, 2);
-% plot(f_axis, ifft(fft_last));
-
-% % Compute the impulse response using the inverse Fourier transform
-% freq_resp = fft_last ./ fft_first;
-% 
-% % Plotting the frequency response
-% figure;
-% plot(f_axis, abs(freq_resp));
-% title('Frequency Response');
-% xlabel('Frequency');
-% ylabel('Magnitude');
+    str = sprintf("\\lambda = %.2f", lambda);
+    
+    subplot(plot_m, plot_n, 1+n);
+    hold on;
+    xlim([pos_last-sigma, pos_last+sigma]);
+    ylim([0, 12]);
+    xlabel("x");
+    ylabel(sprintf("p(x,t=%.1f)", len_t));
+    title(sprintf("Wave packet at t = %.1f - %s", len_t, str));
+    plot(x_axis(round(left_last/dh):round(right_last/dh)), p_last, DisplayName=str);
+end
 
 
 
