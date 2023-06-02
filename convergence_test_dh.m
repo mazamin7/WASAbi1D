fbe4d0cf6283d0ec3a63489cd9af824df52be984
Gerardo Cicalese <92589816+mazamin7@users.noreply.github.com>
2023-06-02 11:57:39 +0200

    Added convergence test dh

---
+++ convergence_test_dh.m
@@ -0,0 +1,114 @@
+clear all, close all, clc;
+
+simulation_parameters = get_simulation_parameters();
+test_case_data = get_test_case();
+c = test_case_data.c0;
+method = simulation_parameters.method_left;
+
+% Extracting test case data
+p_gt_fun = test_case_data.p_gt_fun;
+v_gt_fun = test_case_data.v_gt_fun;
+
+% Simulation parameters
+dt = 1e-3;
+
+if method == 1
+    % FDTD 2ord
+    lambda_arr = [0.02 0.04 0.08 0.1 0.2 0.4 0.8];
+elseif method == 2
+    % FDTD 1ord
+    lambda_arr = [0.01 0.02 0.04 0.08 0.1 0.2 0.4 0.8]/2;
+elseif method == 3
+    % Fourier 2ord
+    lambda_arr = [0.5 1 1.5 2];
+elseif method == 4
+    % Fourier 1ord
+    lambda_arr = [0.5 1 1.5 2];
+end
+
+dh_arr = dt ./ lambda_arr * c;
+
+% artificial dissipation factors for first order
+xi = 1 - 1e-10;
+nu = 1; % 0.99; % in case of Fourier, it only affects DD
+
+% Fourier artificial dissipation factor
+nu_fourier = 1 - 1e-10; % - eps(1); % < 1
+
+L1Err = zeros(length(lambda_arr), 1);
+L2Err = zeros(length(lambda_arr), 1);
+LinfErr = zeros(length(lambda_arr), 1);
+
+for i = 1:length(lambda_arr)
+    dh = dh_arr(i);
+
+    % Run simulation
+    [t_axis, x_axis, p, v] = simulation(test_case_data, simulation_parameters, dt, dh, false, xi, nu, nu_fourier);
+
+    % Evaluate the ground truth on the grid
+    [X, T] = meshgrid(x_axis, t_axis);
+    p_gt = p_gt_fun(X, T)';
+    v_gt = v_gt_fun(X, T)';
+
+    % Compute errors
+    L1Err(i) = norm(p(:)-p_gt(:),1)/numel(p);
+    L2Err(i) = sqrt(norm(p(:)-p_gt(:),2)^2/numel(p));
+    LinfErr(i) = norm(p(:)-p_gt(:),inf);
+end
+
+% Plot errors
+figure;
+subplot(3,1,1);
+plot(dh_arr, L1Err);
+set(gca, 'XScale', 'log', 'XDir', 'reverse');
+hold on;
+
+% Compute theoretical convergence curve
+max_dh = max(dh_arr);
+theory_curve = L1Err(1) * (dh_arr / max_dh);
+plot(dh_arr, theory_curve, 'r-');
+hold off;
+
+title('L1 Error');
+xlabel('dh');
+ylabel('Error');
+legend('Numerical', 'Theory');
+
+subplot(3,1,2);
+plot(dh_arr, L2Err);
+set(gca, 'XScale', 'log', 'XDir', 'reverse');
+hold on;
+
+% Compute theoretical convergence curve
+theory_curve = L2Err(1) * (dh_arr / max_dh);
+plot(dh_arr, theory_curve, 'r-');
+hold off;
+
+title('L2 Error');
+xlabel('dh');
+ylabel('Error');
+legend('Numerical', 'Theory');
+
+subplot(3,1,3);
+plot(dh_arr, LinfErr);
+set(gca, 'XScale', 'log', 'XDir', 'reverse');
+hold on;
+
+% Compute theoretical convergence curve
+theory_curve = LinfErr(1) * (dh_arr / max_dh);
+plot(dh_arr, theory_curve, 'r-');
+hold off;
+
+title('Linf Error');
+xlabel('dh');
+ylabel('Error');
+legend('Numerical', 'Theory');
+
+
+
+
+
+
+
+
+
