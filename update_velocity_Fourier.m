9de55522b906d17ae0367b2f93c39a482f891aeb
Gerardo Cicalese <92589816+mazamin7@users.noreply.github.com>
2023-06-10 01:06:12 +0200

    Minor improvements

--- update_velocity_Fourier.m
+++ update_velocity_Fourier.m
@@ -1,4 +1,4 @@
-function v_next = update_velocity_Fourier(Fourier_data, p_next, p_curr, p_prev, force_now, v_curr, override)
+function v_next = update_velocity_Fourier(Fourier_data, p_next, p_curr, p_prev, force, v_curr, override)
 % Computes p_next given p_curr, p_prev, force and Fourier_data
 %
 % Inputs:
@@ -32,7 +32,7 @@ function v_next = update_velocity_Fourier(Fourier_data, p_next, p_curr, p_prev, 
     p_curr_dct = dct(p_curr,'Type',DCT_type);
     p_next_dct = dct(p_next,'Type',DCT_type);
 
-    force_now_dct = dct(force_now);
+    force_dct = dct(force);
 
     v_curr_dct = dct(v_curr,'Type',DCT_type);
     v_next_dct = zeros(N,1);
@@ -41,13 +41,13 @@ function v_next = update_velocity_Fourier(Fourier_data, p_next, p_curr, p_prev, 
 
     % update solution in Fourier domain
     if order == 2 && override == false
-        % next force
+        % curr force
         v_next_dct(n) = w(n) ./ swt(n) .* (p_next_dct(n) - cwt(n) ...
-            .* p_curr_dct(n)) - inv_w(n) .* tan(w(n) * dt/2) .* force_now_dct(n);
+            .* p_curr_dct(n)) - inv_w(n) .* tan(w(n) * dt/2) .* force_dct(n);
 %         v_next_dct(n) = -w(n) .* swt(n) .* p_curr_dct(n) + cwt(n) .* v_curr_dct(n) + inv_w(n) .* swt(n) .* force_now_dct(n);
     else
         % next force
-        xe = force_now_dct(n) .* inv_w2(n);
+        xe = force_dct(n) .* inv_w2(n);
         v_next_dct(n) = eatm * (v_curr_dct(n) .* (cwt(n) - alpha_abs * inv_w(n) .* swt(n)) - (w(n) + alpha2 * inv_w(n)) .* (p_curr_dct(n) - xe) .* swt(n));
     end
 
@@ -59,7 +59,7 @@ function v_next = update_velocity_Fourier(Fourier_data, p_next, p_curr, p_prev, 
         v_next_dct(n) = (p_next_dct(n) - p_curr_dct(n))/dt;
     else
         % next force
-        v_next_dct(n) = (v_curr_dct(n) + dt * force_now_dct(n)) / (1 + 2 * dt * alpha_abs);
+        v_next_dct(n) = (v_curr_dct(n) + dt * force_dct(n)) / (1 + 2 * dt * alpha_abs);
     end
 
     % perform IDCT
